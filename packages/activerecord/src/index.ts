2cbba90a8aa0989ddab0882a333a9a934578bd8b
yukihirop <te108186@gmail.com>
2021-02-15 01:26:18 +0900

    [activerecord] Rename ActiveRecord Type

--- packages/activerecord/src/index.ts
+++ packages/activerecord/src/index.ts
@@ -5,17 +5,29 @@ export { ActiveRecord$Error } from './errors';
 // functions
 export { errObj } from './errors';
 
-// types
-export type {
-  Association$PrimaryKey,
-  Association$ForeignKey,
-  Association$BelongsTo,
-  Association$HasOne,
-  Association$HasMany,
-  Association$Scope,
-} from './associations';
-export type { Record$Params, Record$ObjType, Record$Validation$Errors } from './records';
-export type { ErrObj as Record$ErrObj, ErrMessage as Record$ErrMessage } from './errors';
-
 // enums
-export type { ErrCodes } from './errors';
+export { ErrCodes } from './errors';
+
+// types
+
+// prettier-ignore
+export type {
+  Association$PrimaryKey as Record$PrimaryKey,
+  Association$ForeignKey as Record$ForeignKey,
+  Association$BelongsTo as Record$BelongsTo,
+  Association$HasOne as Record$HasOne,
+  Association$HasMany as Record$HasMany,
+  Association$Scope as Record$Scope,
+} from './associations';
+
+// prettier-ignore
+export type {
+  Record$Params, Record$ObjType,
+  Record$Validation$Errors
+} from './records';
+
+// prettier-ignore
+export type {
+  ErrObj as Record$ErrObj,
+  ErrMessage as Record$ErrMessage
+} from './errors';
