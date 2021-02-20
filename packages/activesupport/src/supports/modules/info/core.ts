5c04577320f78c1abcbdaedefadca568fe8d33b2
yukihirop <te108186@gmail.com>
2021-02-20 17:22:15 +0900

    [activesupport] Implement getMethodsWithNamespace for repl

--- packages/activesupport/src/supports/modules/info/core.ts
+++ packages/activesupport/src/supports/modules/info/core.ts
@@ -1,4 +1,6 @@
+// types
 import * as mt from '@/modules';
+import * as t from './types';
 
 // this is bound to an instance(class) of Support
 const Info: mt.Module = {
@@ -6,23 +8,65 @@ const Info: mt.Module = {
 
   // https://qiita.com/suin/items/b807769388c54c57a8be
   // @static
-  getMethods(): string[] {
-    const obj = new this();
+  getMethods(obj?: Function): string[] {
+    let target = new this();
+    if (obj) target = obj;
+
     const getOwnMethods = (obj: object) =>
       Object.entries(Object.getOwnPropertyDescriptors(obj))
         .filter(([name, { value }]) => typeof value === 'function' && name !== 'constructor')
         .map(([name]) => name);
+
     const _getMethods = (o: object, methods: string[]): string[] =>
       o === Object.prototype
         ? methods
         : _getMethods(Object.getPrototypeOf(o), methods.concat(getOwnMethods(o)));
-    return _getMethods(obj, []);
+
+    return _getMethods(target, []);
   },
 
   // @static
-  getProperties(): string[] {
-    const obj = new this();
-    const descriptors = Object.getOwnPropertyDescriptors(obj);
+  getMethodsWithNamespace(obj?: Function): string[] {
+    let target = new this();
+    if (obj) target = obj;
+
+    const getOwnMethods = (obj: object): t.MethodWithNamespace =>
+      Object.entries(Object.getOwnPropertyDescriptors(obj))
+        .filter(([name, { value }]) => typeof value === 'function' && name !== 'constructor')
+        .reduce((acc, [name]) => {
+          let namespace = '';
+
+          if (obj.constructor.name === 'Function') {
+            namespace = obj['name'];
+          } else {
+            if (obj['isModule']) {
+              namespace = `${obj.constructor.name} (Module)`;
+            } else {
+              namespace = obj.constructor.name;
+            }
+          }
+
+          if (!acc[namespace]) acc[namespace] = [];
+          acc[namespace].push(name);
+
+          return acc;
+        }, {});
+
+    const _getMethods = (o: object, methods: t.MethodWithNamespace[]): t.MethodWithNamespace[] =>
+      o === Object.prototype
+        ? methods
+        : _getMethods(Object.getPrototypeOf(o), Object.assign(methods, getOwnMethods(o)));
+
+    // @ts-ignore
+    return _getMethods(target, {} as t.MethodWithNamespace);
+  },
+
+  // @static
+  getProperties(obj: Function): string[] {
+    let target = new this();
+    if (obj) target = obj;
+
+    const descriptors = Object.getOwnPropertyDescriptors(target);
     return Object.keys(descriptors);
   },
 };
--- packages/activesupport/src/supports/modules/info/index.ts
+++ packages/activesupport/src/supports/modules/info/index.ts
@@ -1,1 +1,2 @@
 export { Info } from './core';
+export type { MethodWithNamespace as Info$MethodWithNamespace } from './types';
