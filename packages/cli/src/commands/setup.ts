72bcc80608abca43c7bbac2053c15c816c1b6d6d
yukihirop <te108186@gmail.com>
2021-03-01 20:15:46 +0900

    [cli] Implement rue setup <activemodel/activerecord/activeform>

---
+++ packages/cli/src/commands/setup.ts
@@ -0,0 +1,50 @@
+// rue packages
+import { Generator$Packages } from '@rue/generator';
+
+// third party
+import { Command, flags } from '@oclif/command';
+
+// builtin
+import path from 'path';
+
+const projectRoot = require('pkg-dir').sync() || process.cwd();
+const isTypeScript = require('fs').existsSync(path.join(projectRoot, 'tsconfig.json'));
+const defaultExtname = isTypeScript ? 'ts' : 'js';
+export default class Setup extends Command {
+  static description = "Setup '<activerecord/activemodel/activeform>' in your project";
+
+  static flags = {
+    extname: flags.string({
+      char: 'e',
+      description: 'extname',
+      required: false,
+      options: ['js', 'ts'],
+    }),
+    force: flags.boolean({
+      char: 'f',
+      description: 'force update',
+      required: false,
+    }),
+  };
+
+  static args = [
+    {
+      name: 'pkgName',
+      required: true,
+      description: 'package name',
+      options: ['activerecord', 'activemodel', 'activeform'],
+    },
+    { name: 'saveDir', required: false, description: 'save dir path', default: './src/lib' },
+  ];
+
+  async run() {
+    const { flags, args } = this.parse(Setup);
+
+    Generator$Packages.generate({
+      outputDirPath: args.saveDir,
+      pkgName: args.pkgName,
+      extname: (flags.extname || defaultExtname) as 'js' | 'ts',
+      force: flags.force,
+    });
+  }
+}
