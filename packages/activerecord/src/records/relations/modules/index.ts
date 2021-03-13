5cf6549b61968341d72b3beecbd2c5ca24350f7a
yukihirop <te108186@gmail.com>
2021-03-13 12:37:45 +0900

    [activerecord] Remove unused ActiveRecord$QueryMethods$WhereChain

--- packages/activerecord/src/records/relations/modules/index.ts
+++ packages/activerecord/src/records/relations/modules/index.ts
@@ -1,6 +1,5 @@
 export { ActiveRecord$FinderMethods } from './finder_methods';
 export { ActiveRecord$QueryMethods } from './query_methods';
-export { ActiveRecord$QueryMethods$WhereChain } from './query_methods';
 
 // types
 export type { ExistsCondition as FinderMethods$ExistsCondition } from './finder_methods';
--- packages/activerecord/src/records/relations/modules/query_methods/index.ts
+++ packages/activerecord/src/records/relations/modules/query_methods/index.ts
@@ -1,8 +1,6 @@
 // classes
-export { ActiveRecord$QueryMethods$WhereChain } from './where_chain';
 export { ActiveRecord$QueryMethods } from './core';
 export { ActiveRecord$QueryMethods$Evaluator } from './evaluator';
 
 // types
-export type { WhereParams as QueryMethods$WhereParams } from './where_chain';
 export type { Directions, ScopeMethods } from './types';
