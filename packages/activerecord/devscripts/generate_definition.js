2da69726408e578afe17301f7a0bc5a69f3305f1
yukihirop <te108186@gmail.com>
2021-03-20 21:43:48 +0900

    [activerecord] Modify generate_definition script

--- packages/activerecord/devscripts/generate_definition.js
+++ packages/activerecord/devscripts/generate_definition.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { Generator$Definitions } = require('@rue/generator');
 
-Generator$Definitions.generate(['src/**/*.ts', '!src/**/__tests__/*.test.ts'], {
+Generator$Definitions.generate(['src/**/*.ts', '!src/**/__tests__/**/*.test.ts'], {
   pkgName: 'activerecord',
   outputPath: '../../packages/definition/src/activerecord.json',
   cwd: path.resolve('.'),
