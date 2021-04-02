b862efb62d3ecbfb4251d95e4960beb4babb60c7
yukihirop <te108186@gmail.com>
2021-04-03 04:26:00 +0900

    [generator][examples] Update components template

--- packages/examples/src/generate/records/User.js
+++ packages/examples/src/generate/records/User.js
@@ -26,13 +26,93 @@ export class User extends ActiveRecord {
     return 'User';
   }
 
-
   /**
    * @protected
    * @return {Promise<Array<property>>}
    */
   fetchAll() {
     throw "Please override 'fetchAll()'";
+  }
+
+  /**
+   * @param {{ validate: boolean }} opts
+   * @return {Promise<boolean>}
+   */
+  save(opts = undefined) {
+    throw "Please override 'save' to hit the external API."
+  }
+
+  /**
+   * @return {Promise<void | boolean>}
+   */
+  saveOrThrow() {
+    throw "Please override 'saveOrThrow' to hit the external API."
+  }
+
+  /**
+   * @return {Promise<ActiveRecord$Base>}
+   */
+  destroy() {
+    throw "Please override 'destroy' to hit the external API."
+  }
+
+  /**
+   * @param {object} params
+   * @return {Promise<boolean>}
+   */
+  update(params = undefined) {
+    throw "Please override 'update' to hit the external API."
+  }
+
+  /**
+   * @param {object} params
+   * @return {Promise<boolean>}
+   */
+  updateOrThrow(params = undefined) {
+    throw "Please override 'updateOrThrow' to hit the external API."
+  }
+
+  /**
+   * @param {object | Array<object>} params
+   * @param {(self) => void} yielder
+   * @return {Promise<T | T[]>}
+   */
+  static create(params = undefined, yielder = undefined) {
+    throw "Please override 'static create' to hit the extenral API."
+  }
+
+  /**
+   * @param {object | Array<object>} params
+   * @param {(self) => void} yielder
+   * @return {Promise<T | T[]>}
+   */
+  static createOrThrow(params = undefined, yielder = undefined) {
+    throw "Please override 'static createOrThrow' to hit the external API."
+  }
+
+  /**
+   * @param {stirng|number|Array<string|number>} id
+   * @return {Promise<number>}
+   */
+  static delete(id) {
+    throw "Please override 'static delete' to hit the external API."
+  }
+
+  /**
+   * @param {string|number|Array<string|number>} id
+   * @return {Promise<T | T[]>}
+   */
+  static destroy(id) {
+    throw "Please override 'static destroy' to hit the external API."
+  }
+
+  /**
+   * @param {string|number|Array<string|number>| 'all'} id
+   * @param {object|Array<object>} params
+   * @return {Promise<T | T[]>}
+   */
+  static update(id, params) {
+    throw "Please override 'static update' to hit the external API."
   }
 }
 
--- packages/examples/src/generate/records/User.ts
+++ packages/examples/src/generate/records/User.ts
@@ -33,7 +33,59 @@ export class User extends ActiveRecord<UserParams> {
   }
 
   protected fetchAll(): Promise<UserParams[]> {
-    throw "Please override 'fetchAll()'";
+    throw "Please implement 'protected fetchAll(): Promise<T[]>' in Inherited Class";
+  }
+  save(opts?: { validate: boolean }): Promise<boolean> {
+    throw "Please override 'save' to hit the external API."
+  }
+
+  saveOrThrow(): Promise<void | boolean> {
+    throw "Please override 'saveOrThrow' to hit the external API."
+  }
+
+  destroy<T extends ActiveRecord$Base>(): Promise<T> {
+    throw "Please override 'destroy' to hit the external API."
+  }
+
+  update<U>(params?: Partial<U>): Promise<boolean> {
+    throw "Please override 'update' to hit the external API."
+  }
+
+  updateOrThrow<U>(params?: Partial<U>): Promise<boolean> {
+    throw "Please override 'updateOrThrow' to hit the external API."
+  }
+
+  static create<T extends ActiveRecord$Base, U>(
+    params?: Partial<U> | Array<Partial<U>>,
+    yielder?: (self: T) => void
+  ): Promise<T | T[]> {
+    throw "Please override 'static create' to hit the extenral API."
+  }
+
+  static createOrThrow<T extends ActiveRecord$Base, U>(
+    params?: Partial<U> | Array<Partial<U>>,
+    yielder?: (self: T) => void
+  ): Promise<T | T[]> {
+    throw "Please override 'static createOrThrow' to hit the external API."
+  }
+
+  static delete<T extends ActiveRecord$Base>(
+    id: t.Record$PrimaryKey | t.Record$PrimaryKey[]
+  ): Promise<number> {
+    throw "Please override 'static delete' to hit the external API."
+  }
+
+  static destroy<T extends ActiveRecord$Base>(
+    id: t.Record$PrimaryKey | t.Record$PrimaryKey[]
+  ): Promise<T | T[]> {
+    throw "Please override 'static destroy' to hit the external API."
+  }
+
+  static update<T extends ActiveRecord$Base, U>(
+    id: at.Associations$PrimaryKey | at.Associations$PrimaryKey[] | 'all',
+    params: Partial<U> | Array<Partial<U>>
+  ): Promise<T | T[]> {
+    throw "Please override 'static update' to hit the external API."
   }
 }
 
