d9ad0a6d8c744b806287d0c1b11aeedce5cbce0e
yukihirop <te108186@gmail.com>
2021-04-06 00:10:00 +0900

    [examples] Update examples

--- packages/examples/rue.config.js
+++ packages/examples/rue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  backend: {
+    mock_server: {
+      loadData: ['./backend/mock_server/data/**/*.{js,json}'],
+      dist: {
+        db: './backend/mock_server/db.json',
+        routes: './backend/mock_server/routes.json',
+      },
+    },
+  },
   cli: {
     commands: {
       console: {
@@ -13,7 +22,7 @@ module.exports = {
     loadModules: [
       'src/**/{forms,models,records}/**/*.ts',
       'src/setup/rc.ts',
-      '!src/**/__tests__/*.test.{js,ts}'
+      '!src/**/__tests__/*.test.{js,ts}',
     ],
     moduleAliases: {
       '@': './src',
