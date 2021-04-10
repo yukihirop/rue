4dd8fdf365a6eee781665955bdc34b652e5d181c
yukihirop <te108186@gmail.com>
2021-04-10 18:10:20 +0900

    [examples] Fix examples

--- packages/examples/src/lib/activemodel.ts
+++ packages/examples/src/lib/activemodel.ts
@@ -1,17 +1,4 @@
 import { ActiveModel$Base, RueSetup } from '@rue/rue';
-import { resources } from '@/locales';
-
-// types
-import type * as t from '@rue/rue';
 
 @RueSetup
-export class ActiveModel extends ActiveModel$Base {
-  static i18nConfig(): t.Model$I18nConfig {
-    return {
-      options: {
-        lng: 'ja',
-      },
-      resources,
-    };
-  }
-}
+export class ActiveModel extends ActiveModel$Base {}
